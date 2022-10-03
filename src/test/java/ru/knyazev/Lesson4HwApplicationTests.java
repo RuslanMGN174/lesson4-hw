f8c087cd7fbd458012b59ad32a1aaf8cdb494f73
Knyazev Ruslan <rusland174@gmail.com>
2022-10-04 00:05:42 +0500

    Initial commit

---
+++ src/test/java/ru/knyazev/Lesson4HwApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.knyazev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lesson4HwApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
